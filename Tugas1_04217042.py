c97a763b2b4eca8d52d67ba60ca5e77ae3a9572c
Memisme <31976149+Memisme@users.noreply.github.com>
2017-09-22 09:23:24 +0700

    Add files via upload
    tugas 1 selesai

---
+++ Tugas1_04217042.py
@@ -0,0 +1,11 @@
+nama = "nama : Devin Anandra "
+nim 	= "nim  : 04217042 "
+umur = 18
+x = (umur + 5)
+
+print (nama)
+print (nim)
+print "umur"
+print (umur)
+print "umur setelah lima tahun"
+print (x)
