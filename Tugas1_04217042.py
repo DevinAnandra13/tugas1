ca580aba1574921e8230625ebdcabf0bbdbe8680
Memisme <31976149+Memisme@users.noreply.github.com>
2017-10-13 09:52:59 +0700

    Update Tugas1_04217042.py

--- Tugas1_04217042.py
+++ Tugas1_04217042.py
@@ -4,7 +4,7 @@ umur = 18
 x = (umur + 5)
 
 print ("nama :" + nama)
-print (nim : " + nim)
+print ("nim : " + nim)
 print "umur"
 print ("umur : " , umur)
 print ("umur setelah lima tahun : " , umur + 5) 
